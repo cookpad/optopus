86b6bd3eca3c6e5def18a7760c41d325d49c0d32
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2011-05-31 21:35:29 +0900

    etc...

---
+++ test.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'lib/optopus'
+
+opts = optopus do
+  desc 'print lots of debugging information'
+  option :debug, '-d', '--debug'
+
+  desc 'log messages to FILE.'
+  option :output_file, '-o', '--output-file FILE', :default => '/var/log/xxx.log'
+
+  desc 'set number of retries to NUMBER (0 unlimits)'
+  option :tries, '-t', '--tries NUMBER', :type => Integer, :default => 0 do
+    # custom validation
+    invalid_argument if @value < 0
+  end
+
+  desc 'comma-separated list of accepted extensions'
+  option :accept, '-A', '--accept LIST', :type => Array, :default => []
+
+  desc 'access timestamp'
+  option :timestamp, '-T', '--timestamp TIME', :type => Time
+
+  # read yaml config file and overwrite options
+  file '-c', '--config-file FILE'
+
+  after do
+    # postprocessing
+    # @options.each { ... }
+  end
+
+  exception do |e|
+    $stderr.puts e.message
+    exit 1
+  end
+end
+
+p opts
